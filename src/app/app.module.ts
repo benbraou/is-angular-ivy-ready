f9b13506d2b7b8082c9e67443143d67bf0cb9e05
Oussama Ben Brahim <benbraou@gmail.com>
2018-04-08 00:15:42 +0200

    add empty navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, NavbarComponent],
+  imports: [BrowserModule, FlexLayoutModule],
   providers: [],
   bootstrap: [AppComponent],
 })
