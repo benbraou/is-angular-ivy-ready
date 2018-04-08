e95382dc6f0a1ad7d174b1cb53a5d8cc69769d87
Oussama Ben Brahim <benbraou@gmail.com>
2018-04-08 21:57:28 +0200

    add theming based on angular aio

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,27 +1,9 @@
 import { NgModule } from '@angular/core';
 
-import {
-  MatButtonModule,
-  MatMenuModule,
-  MatToolbarModule,
-  MatIconModule,
-  MatCardModule,
-} from '@angular/material';
+import { MatToolbarModule } from '@angular/material';
 
 @NgModule({
-  imports: [
-    MatButtonModule,
-    MatMenuModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatCardModule,
-  ],
-  exports: [
-    MatButtonModule,
-    MatMenuModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatCardModule,
-  ],
+  imports: [MatToolbarModule],
+  exports: [MatToolbarModule],
 })
 export class MaterialModule {}
