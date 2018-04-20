1bb78eb3c96b2f64d6ba1266e3e7b6a4230e0dea
Oussama Ben Brahim <benbraou@gmail.com>
2018-04-20 20:52:06 +0200

    add the model of ivy API response

---
+++ src/app/models/response.model.ts
@@ -0,0 +1,53 @@
+export class Response {
+  API: API;
+  errors: Error[];
+  data: ApiData;
+  links: { [key: string]: string };
+}
+
+export class API {
+  version: string;
+}
+
+export class ErrorSource {
+  pointer: string;
+}
+
+export class Error {
+  code: string;
+  source: ErrorSource;
+  title: string;
+  detail: string;
+}
+
+export class FeatureGroup {
+  data: FeatureGroupData;
+  featureGroups: FeatureGroup[];
+}
+
+export class FeatureGroupData {
+  name: string;
+  features: Feature[];
+}
+
+export class Feature {
+  name: string;
+  status: FeatureStatus;
+  childFeatures: Feature[];
+}
+
+export class FeatureStatus {
+  completed: boolean;
+  granularStatuses: GranularStatus[];
+}
+
+export class GranularStatus {
+  category: string;
+  code: string;
+  description: string;
+}
+
+export class ApiData {
+  lastUpdateDate: string;
+  featureGroup: FeatureGroup;
+}
