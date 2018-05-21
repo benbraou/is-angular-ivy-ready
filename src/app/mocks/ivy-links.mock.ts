c6ea761153318349b7c6aed633b43bf526a47c9e
Oussama Ben Brahim <benbraou@gmail.com>
2018-05-21 17:22:02 +0200

    add docs link to Ivy change detection

--- src/app/mocks/ivy-links.mock.ts
+++ src/app/mocks/ivy-links.mock.ts
@@ -7,6 +7,12 @@ export const IVY_LINKS: IvyLink[] = [
     url: 'https://youtu.be/dIxknqPOWms?t=1359',
   },
   {
+    shortDescription: 'Angular Ivy change detection execution',
+    fullDescription: 'Angular Ivy change detection execution',
+    url:
+      'https://blog.angularindepth.com/angular-ivy-change-detection-execution-are-you-prepared-ab68d4231f2c',
+  },
+  {
     shortDescription: 'Ivy status',
     fullDescription: 'Ivy status',
     url:
@@ -23,11 +29,5 @@ export const IVY_LINKS: IvyLink[] = [
       'This document details the new architecture of the Angular compiler in a post-Ivy world',
     url:
       'https://github.com/angular/angular/blob/master/packages/compiler/design/architecture.md',
-  },
-  {
-    shortDescription: 'Design doc(ivy): Separate Compilation',
-    fullDescription: 'Design doc(ivy): Separate Compilation',
-    url:
-      'https://github.com/angular/angular/blob/master/packages/compiler/design/separate_compilation.md',
   },
 ];
